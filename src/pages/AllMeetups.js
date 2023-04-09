1e8a201e77d65ddc71d34d19842aeadd287ff934
Catalin <catalintium@gmail.com>
2023-04-09 13:42:39 +0300

    basic hard coded version

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,15 +1,31 @@
+import MeetupList from "../components/meetups/MeetupList";
+
 const DUMMY_DATA = [
-{id: "m1", title:"This is the first meet", image:'https://ro.wikipedia.org/wiki/Fi%C8%99ier:Pia%C8%9Ba_Victoriei_Timi%C8%99oara.jpg', address:"Str. Iliesu12", description:"Amaing experience"},
-{id: "m2", title:"This is the second meet", image:'https://ro.wikipedia.org/wiki/Fi%C8%99ier:Pia%C8%9Ba_Victoriei_Timi%C8%99oara.jpg', address:"Str. Iliesu34", description:"Bad experience"}
+  {
+    id: "m1",
+    title: "This is the first meet",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/e/ef/Pia%C8%9Ba_Victoriei_Timi%C8%99oara.jpg",
+    address: "Str. Iliesu12",
+    description: "Amaing experience",
+  },
+  {
+    id: "m2",
+    title: "This is the second meet",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/e/ef/Pia%C8%9Ba_Victoriei_Timi%C8%99oara.jpg",
+    address: "Str. Iliesu34",
+    description: "Bad experience",
+  },
 ];
 
 function AllMeetupsPage() {
-  return <section>
-    <h1>All Meetups</h1>
-    {DUMMY_DATA.map((meetup) => {
-      return <li key={meetup.id}>{meetup.title}</li>;
-    })}
-  </section>;
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={DUMMY_DATA} />
+    </section>
+  );
 }
 
 export default AllMeetupsPage;
